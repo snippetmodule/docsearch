adc1a27ce85f5403ace8646a43c5d622423171a0
wangfaxi <wangfaxi@cmcm.com>
2016-06-01 03:01:34 +0000

    修改.sh

--- bin/start.sh
+++ bin/start.sh
@@ -4,17 +4,19 @@ cd `dirname $0`
 cd ..
 dir=`pwd`
 
-# 安装 依赖
+echo "安装 依赖:------------------------------------------------"
 npm install
 
-# 停止当前 forever 进程
+echo "停止当前 forever 进程:------------------------------------"
 ./node_modules/.bin/forever stop src/www
 
-# 开发环境下启动
-#NODE_ENV=development $dir/node_modules/.bin/forever start -w --watchIgnore "*.log" -a -l $dir/logs/server/forever.log -o $dir/logs/server/out.log -e $dir/logs/server/err.log $dir/bin/www
+mkdir $dir/logs/server
 
-# 线上环境下启动
-NODE_ENV=production ./node_modules/.bin/forever start -w --watchIgnore "*.log" -a -l $dir/logs/server/forever.log -o $dir/logs/server/out.log -e $dir/logs/server/err.log src/www
+echo "开发环境下启动:--------------------------------------------"
+NODE_ENV=development $dir/node_modules/.bin/forever start -w --watchIgnore "*.log" -a -l $dir/logs/server/forever.log -o $dir/logs/server/out.log -e $dir/logs/server/err.log src/www
 
-# forever 正在运行进程列表
-#./node_modules/.bin/forever list
+echo "线上环境下启动:--------------------------------------------"
+#NODE_ENV=production ./node_modules/.bin/forever start -w --watchIgnore "*.log" -a -l $dir/logs/server/forever.log -o $dir/logs/server/out.log -e $dir/logs/server/err.log src/www
+
+echo "forever 正在运行进程列表:----------------------------------"
+./node_modules/.bin/forever list
--- bin/stop.sh
+++ bin/stop.sh
@@ -4,5 +4,5 @@ cd `dirname $0`
 cd ..
 dir=`pwd`
 
-# 停止当前 forever 进程
+echo "停止当前 forever 进程:------------------------------------"
 ./node_modules/.bin/forever stop src/www
